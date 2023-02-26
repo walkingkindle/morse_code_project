ecfd83be89d0877067543b47bf329f826e97549b
Alex <aki.hadza@gmail.com>
2023-02-26 12:33:58 +0100

    adding the reverse: Converting morse code into text

--- engine.py
+++ engine.py
@@ -10,17 +10,42 @@ class Engine:
     '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
     '8': '---..', '9': '----.', ' ': '/'
 }
-        self.text = input("Tell me what you want to translate. ")
+        self.morse_code_reversed ={
+        '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+        '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+        '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+        '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+        '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+        '--..': 'Z', '-----': '0', '.----': '1', '..---': '2', '...--': '3',
+        '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8',
+        '----.': '9', '/': ' '
+    }
+        self.first_question = input("Type '1': I want to convert text into morse code.\n"
+                                    "Type '2': I want to convert morse code into text.")
+        if self.first_question == "1":
+            self.text = input("Tell me what you want to translate. ")
+        else:
+            self.text_m = input("Type your morse code here. ")
         self.morse_code_text_list = []
         self.morse_code_text = ""
+        self.new_text = ""
+        self.complete = ""
+
+
 
     def text_to_morse(self):
-        """Converts Morse code to text."""
+        """Converts text to morse code."""
         for word in self.text:
             for char in word:
                 if char.upper() in self.morse_code:
                     new_char = self.morse_code[char.upper()]
                     self.morse_code_text += new_char + ","
-                    print(self.morse_code_text[:-1])
         return self.morse_code_text[:-1]
 
+    def morse_to_text(self):
+        """Converts morse code to text."""
+        ready_text = self.text_m.split(' ')
+        for character in ready_text:
+            new_character = self.morse_code_reversed[character]
+            self.complete += new_character
+        return print(self.complete.strip())
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from engine import Engine
 from playsound import playsound
+from gtts import gTTS
+import os
 
-
+language = 'en'
 eng = Engine()
 #character equals that same character but in morse code.
 #character
-if eng.text:
-    eng.text_to_morse()
-    real_morse = eng.morse_code_text.replace(',',' ')
-    print(real_morse)
-    for char in real_morse:
-        if char == '.':
-            playsound(r"C:\Users\Aleksa Hadzic\PycharmProjects\morse_code_project\sounds\dot.mp3")
-        elif char == '-':
-            playsound(r"C:\Users\Aleksa Hadzic\PycharmProjects\morse_code_project\sounds\dash.mp3")
-
+if eng.first_question == '1':
+    if eng.text:
+        eng.text_to_morse()
+        real_morse = eng.morse_code_text.replace(',',' ')
+        print(real_morse)
+        for char in real_morse:
+            if char == '.':
+                playsound(r"C:\Users\Aleksa Hadzic\PycharmProjects\morse_code_project\sounds\dot.mp3")
+            elif char == '-':
+                playsound(r"C:\Users\Aleksa Hadzic\PycharmProjects\morse_code_project\sounds\dash.mp3")
+elif eng.first_question == '2':
+    eng.morse_to_text()
